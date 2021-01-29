2969e0c495f6a7775b4ae06b230a11d7a99a2e46
nurjanat <tungatarovva@gmail.com>
2021-01-29 15:02:31 +0300

    create path

--- pizza/products/urls.py
+++ pizza/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,AboutUs_page,Contacts_page
 
 urlpatterns = [
-    path('products/',homepage)
+    path('products/',homepage),
+    path('about_us/',AboutUs_page),
+    path('contacts/',Contacts_page)
 ]
+
