027e4e65875f50022eeefe2e539cce7e0fb78538
nurjanat <tungatarovva@gmail.com>
2021-01-29 14:57:24 +0300

    register models in admin panel

--- pizza/products/admin.py
+++ pizza/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import Product,AboutUs,Contacts
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(AboutUs)
+admin.site.register(Contacts)
