08ee2e2aa5ca9de081662998b62ca789ac9cf029
nurjanat <tungatarovva@gmail.com>
2021-01-29 15:01:30 +0300

    create functions

--- pizza/products/views.py
+++ pizza/products/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from.models import Product,AboutUs,Contacts
 
 # Create your views here.
 
 def homepage(request):
-    return render(request,'products/products.html')
+    products = Product.objects.all()
+    return render(request,'products/products.html',{"products":products})
+
+
+
+
+
+
+
+def AboutUs_page(request):
+    abouts = AboutUs.objects.all()
+    return  render(request,'products/aboutus.html',{"about us":abouts})
+
+def Contacts_page(request):
+    contacts = Contacts.objects.all()
+    return  render(request,'products/contacts.html',{"contacts":contacts})
